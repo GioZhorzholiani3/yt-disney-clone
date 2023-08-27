296c467c2905db11d441d88d7471875a85df2072
GioZhorzholiani3 <giorgi.zhorzholiani.1@btu.edu.ge>
2023-08-27 14:54:56 +0400

    add more on home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 import ImgSlider from "./ImgSlider";
+import Viewers from "./Viewers";
+import Recommends from "./Recommends";
+import DisneyPlus from "./DisneyPlus";
+import Originals from "./Originals";
+import Trending from "./Trending";
 
 const Container = styled.section`
   position: relative;
@@ -24,6 +29,11 @@ const Home = () => {
   return (
     <Container>
       <ImgSlider />
+      <Viewers />
+      <Recommends />
+      <DisneyPlus />
+      <Originals />
+      <Trending />
     </Container>
   );
 };
